275865a950654defd8196d4d19d0b60266e02e8c
mehari123 <88460961+mehari123@users.noreply.github.com>
2022-11-02 10:48:22 +0300

    Delete 0x17-web_stack_debugging_4 directory

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,4 +0,0 @@
-# puppet script to debug web server failed requests
-exec { 'increase limit on open files':
-  command => '/bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"- Hn\"/" /etc/default/nginx && /etc/init.d/nginx restart',
-  }
